0f881aae2c26c3f254a3fb24f0d8c98a897abcd4
marianedinis <marianedinis@gmail.com>
2018-08-14 20:29:30 +1000

    [Mariane] [#0] added Mariane in the teamPage template and added tests

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -12,11 +12,14 @@ describe('TeamPage', () => {
       const page = new TeamPage();
       expect(page.template()).toContain("<h1>Made by:</h1>");
     });
+
     it('should contain team members names', () => {
       const page = new TeamPage();
       expect(page.template()).toContain("Priyam");
       expect(page.template()).toContain("Matt");
       expect(page.template()).toContain("Kaia");
+      expect(page.template()).toContain("Mariane");
+
     });
   });
 
